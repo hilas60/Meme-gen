11a4816cdaf29e6e23624bbcb4279d40b19ab2d0
Hila S <hilas60@gmail.com>
2019-10-23 12:57:15 +0300

    added draw image on canvas

--- js/main.js
+++ js/main.js
@@ -1,1 +1,29 @@
 'use strict'
+
+const image = document.querySelector('#img-source');
+const gCanvas = document.querySelector('#main-canvas');
+const gCtx = gCanvas.getContext('2d');
+
+
+
+function init(){
+    resizeCanvas();
+    drawImg()
+}
+
+function resizeCanvas() {
+    var elContainer = document.querySelector('.canvas-container');
+    gCanvas.width = elContainer.offsetWidth
+    // console.log(gCanvas.width);
+    gCanvas.height = elContainer.offsetHeight
+    // console.log(gCanvas.height);
+}
+
+function drawImg() {
+    let imgUrl = getImg(1);
+    image.src = imgUrl;
+    gCanvas.width = image.width;
+    gCanvas.height = image.height;
+    // var img = document.querySelector('img');
+    image.onload = gCtx.drawImage(image, 0, 0);
+}
--- js/meme-service.js
+++ js/meme-service.js
@@ -1,1 +1,21 @@
 'use strict'
+
+let gImgs = [{id: 1, url:'img/1.jpg'}, {id: 2, url:'img/2.jpg'}];
+let gMeme = {
+    selectedImgId: 1, 
+    selectedTxtId: 1, 
+    txts: [
+        {line: 'I love dogs',
+         size: 1,
+         align: 'left',
+         color: 'black',
+        }
+    ],
+}
+
+function getImg() {
+    let img = gImgs.find(function(img){
+        return img.id === gMeme.selectedImgId;
+    })
+    return img.url;
+}
