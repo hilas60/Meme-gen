975af167dff2c397f3e07762407ae08f1f409c24
Hila S <hilas60@gmail.com>
2019-10-28 10:03:07 +0200

    some fixes

--- js/editor-main.js
+++ js/editor-main.js
@@ -6,43 +6,45 @@ const gCtx = gCanvas.getContext('2d');
 
 
 function init() {
-    resizeCanvas(gCanvas);
+    resizeCanvas(gCanvas)
     setSelectedImgId();
     renderMeme()
 }
 
 function resizeCanvas(canvas) {
     var elContainer = document.querySelector('.canvas-container');
-    canvas.height = elContainer.offsetHeight;
-    // canvas.width = elContainer.offsetWidth
+    console.log(elContainer.offsetWidth);
+
+    canvas.width = elContainer.offsetWidth;
+    canvas.height = elContainer.offsetWidth;
     // let aspectRatio
-    // canvas.width = window.innerWidth
+    // canvas.width = image.naturalWidth;
     // canvas.height = window.innerHeight
 }
 
-function renderMeme(){
-    let meme = getMeme();
+function renderMeme() {
+    // let meme = getMeme();
     var imgUrl = getImgUrl();
     var image = new Image();
+    image.src = imgUrl;
     image.onload = function () {
         // Draw Image and text
-        gCtx.drawImage(image, 0, 0, gCanvas.width ,gCanvas.height );
+        gCtx.drawImage(image, 0, 0, gCanvas.width, gCanvas.height);
         drawTxts();
     }
-    image.src = imgUrl;
+
+    console.log(image.width);
 }
 
 function drawTxts() {
     let txts = getMeme().txts;
-    txts.forEach(function(txt){
+    txts.forEach(function (txt) {
         let fontSize = '' + txt.size;
-        // let fontFamily = gTxt.fontFamily;
         gCtx.font = fontSize + 'em impact';
         gCtx.lineWidth = 2;
         gCtx.textAlign = txt.align;
         gCtx.fillStyle = txt.color;
         gCtx.strokeStyle = 'black';
-        // let txtLine = txt[txtIdx].line
         gCtx.fillText(txt.line, txt.x, txt.y);
         gCtx.strokeText(txt.line, txt.x, txt.y);
     })
@@ -63,7 +65,7 @@ function onChangeFontSize(elBtn) {
 
 function onMoveLine(elBtn) {
     let direction
-    if (elBtn.classList.contains('up-btn')){
+    if (elBtn.classList.contains('up-btn')) {
         direction = 'up'
     } else {
         direction = 'down'
@@ -72,7 +74,7 @@ function onMoveLine(elBtn) {
     renderMeme();
 }
 
-function onSwitchLines() {    
+function onSwitchLines() {
     var activeTxt = switchLines();
     console.log(activeTxt);
     // TODO - change text in input according to activeTxt;
@@ -87,17 +89,107 @@ function toggleMenu() {
     elMenuBtn.classList.toggle('open-menu');
 }
 
-function onAlignTxt(elBtn){
+function onAlignTxt(elBtn) {
     setAlignTxt(elBtn.id);
     renderMeme();
 }
 
-function onAddLine(params) {
-    console.log('Adding a line');
+function onAddLine() {
+    createTxtLine(gCanvas.width);
+    renderMeme();
 }
 
-function onRemoveLine(params) {
-    console.log('Removing');
+function onRemoveLine() {
     removeLine();
     renderMeme();
 }
+
+function onDownloadMeme(elLink) {
+    var imgContent = gCanvas.toDataURL('image/jpeg');
+    elLink.href = imgContent
+}
+
+// function onShareMeme(width, height) {
+//     var leftPosition, topPosition;
+//     //Allow for borders.
+//     leftPosition = (window.screen.width / 2) - ((width / 2) + 10);
+//     //Allow for title and status bars.
+//     topPosition = (window.screen.height / 2) - ((height / 2) + 50);
+//     var windowFeatures = "status=no,height=" + height + ",width=" + width + ",resizable=yes,left=" + leftPosition + ",top=" + topPosition + ",screenX=" + leftPosition + ",screenY=" + topPosition + ",toolbar=no,menubar=no,scrollbars=no,location=no,directories=no";
+//     u=location.href;
+//     t=document.title;
+//     window.open('http://www.facebook.com/sharer.php?u='+encodeURIComponent(u)+'&t='+encodeURIComponent(t),'sharer', windowFeatures);
+//     return false;
+// }
+
+// function onShareMeme(elShareBtn) {
+//     console.log(elShareBtn);
+//     var imgContent = gCanvas.toDataURL('image/jpeg');
+//     elShareBtn.innerHtml = `<a class="w-inline-block social-share-btn btn fb" href="https://www.facebook.com/sharer/sharer.php?u=${imgContent}&t=${imgContent}" title="Share on Facebook" target="_blank" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u=${imgContent}&t=${imgContent}'); return false;">
+//     </a>`
+//     // var imgContent = gCanvas.toDataURL('image/jpeg');
+
+//     (function(d, s, id) {
+//         var js, fjs = d.getElementsByTagName(s)[0];
+//         if (d.getElementById(id)) return;
+//         js = d.createElement(s); js.id = id;
+//         js.src = 'https://connect.facebook.net/he_IL/sdk.js#xfbml=1&version=v3.0&appId=807866106076694&autoLogAppEvents=1';
+//         fjs.parentNode.insertBefore(js, fjs);
+//       }(document, 'script', 'facebook-jssdk'));
+// }
+
+// Share meme -
+
+// function uploadImg(elForm, ev) {
+//     ev.preventDefault();
+//     document.getElementById('imgData').value = gCanvas.toDataURL("image/jpeg");
+//     // A function to be called if request succeeds
+//     function onSuccess(uploadedImgUrl) {
+//         uploadedImgUrl = encodeURIComponent(uploadedImgUrl)
+//         document.querySelector('.share-container').innerHTML = `
+//         <a class="w-inline-block social-share-btn btn fb" href="https://www.facebook.com/sharer/sharer.php?u=${uploadedImgUrl}&t=${uploadedImgUrl}" title="Share on Facebook" target="_blank" onclick="window.open('https://www.facebook.com/sharer/sharer.php?u=${uploadedImgUrl}&t=${uploadedImgUrl}'); return false;">
+//            Share   
+//         </a>`
+//     }
+//     doUploadImg(elForm, onSuccess);
+// }
+
+// function doUploadImg(elForm, onSuccess) {
+//     var formData = new FormData(elForm);
+//     fetch('http://ca-upload.com/here/upload.php', {
+//         method: 'POST',
+//         body: formData
+//     })
+//         .then(function (response) {
+//             return response.text()
+//         })
+//         .then(onSuccess)
+//         .catch(function (error) {
+//             console.error(error)
+//         })
+// }
+
+// // facebook api
+// (function (d, s, id) {
+//     var js, fjs = d.getElementsByTagName(s)[0];
+//     if (d.getElementById(id)) return;
+//     js = d.createElement(s); js.id = id;
+//     js.src = 'https://connect.facebook.net/he_IL/sdk.js#xfbml=1&version=v3.0&appId=807866106076694&autoLogAppEvents=1';
+//     fjs.parentNode.insertBefore(js, fjs);
+// }(document, 'script', 'facebook-jssdk'));
+
+// // The next 2 functions handle IMAGE UPLOADING to img tag from file system: 
+// function onImgInput(ev) {
+//     loadImageFromInput(ev, renderCanvas)
+// }
+// function loadImageFromInput(ev, onImageReady) {
+//     document.querySelector('.share-container').innerHTML = ''
+//     var reader = new FileReader();
+
+//     reader.onload = function (event) {
+//         var img = new Image();
+//         img.onload = onImageReady.bind(null, img)
+//         img.src = event.target.result;
+//     }
+//     reader.readAsDataURL(ev.target.files[0]);
+// }
--- js/meme-service.js
+++ js/meme-service.js
@@ -1,8 +1,7 @@
 'use strict'
 
 const IMAGE_ID_KEY = 'image-Id'
-
-let gImgs = [{ id: 1, url: 'img/1.jpg' }, { id: 2, url: 'img/2.jpg' }];
+            
 let gMeme = {
     selectedImgId: 1,
     selectedTxtIdx: 0,
@@ -10,20 +9,36 @@ let gMeme = {
         {
             line: 'Text 1',
             size: 2,
-            align: 'center',
+            align: 'right',
             color: 'white',
-            x: 100,
+            x: 200,
             y: 100,
         },
         {
             line: 'Text 2',
             size: 2,
-            align: 'center',
+            align: 'left',
             color: 'red',
-            x: 100,
+            x: 200,
             y: 350,
         }
     ],
+}
+
+// function getImagesFromFolder (){
+
+// }
+
+function createTxtLine(width) {
+    var newLine = {
+        line: 'Enter text here',
+        size: 2,
+        align: 'center',
+        color: 'white',
+        x: 200,
+        y: 200,
+    }
+    gMeme.txts.push(newLine);
 }
 
 function getImgUrl() {
@@ -45,13 +60,13 @@ function changeTxt(txt) {
 function changeFontSize(fontChange) {
     let txtIdx = gMeme.selectedTxtIdx;
     let txtObj = gMeme.txts[txtIdx];
-        (fontChange) ? txtObj.size++ : txtObj.size--;
+    (fontChange) ? txtObj.size++ : txtObj.size--;
 }
 
 function switchLines() {
     let txtIdx = gMeme.selectedTxtIdx;
     let txtsLength = gMeme.txts.length;
-    if (txtIdx > -1 && txtIdx < txtsLength-1){
+    if (txtIdx > -1 && txtIdx < txtsLength - 1) {
         gMeme.selectedTxtIdx++;
     } else {
         gMeme.selectedTxtIdx = 0;
@@ -64,7 +79,7 @@ function changeImgId(imgId) {
     saveImgIdToStorage()
 }
 
-function setSelectedImgId(){
+function setSelectedImgId() {
     gMeme.selectedImgId = loadImgIdFromStorage();
 }
 
@@ -72,15 +87,15 @@ function getImages() {
     return gImgs;
 }
 
-function saveImgIdToStorage (){
+function saveImgIdToStorage() {
     saveToLocalStorage(IMAGE_ID_KEY, gMeme.selectedImgId);
 }
 
-function loadImgIdFromStorage(){
+function loadImgIdFromStorage() {
     return +loadFromLocalStorage(IMAGE_ID_KEY);
 }
 
-function setAlignTxt(alignment){
+function setAlignTxt(alignment) {
     let txtIdx = gMeme.selectedTxtIdx
     gMeme.txts[txtIdx].align = alignment;
 }
@@ -88,9 +103,38 @@ function setAlignTxt(alignment){
 function removeLine() {
     var txtIdx = gMeme.selectedTxtIdx;
     gMeme.txts.splice(txtIdx, 1);
+    (gMeme.selectedTxtIdx !== 0) ? gMeme.selectedTxtIdx-- : gMeme.selectedTxtIdx = 0
 }
 
 function moveLine(direction) {
     let txtIdx = gMeme.selectedTxtIdx;
-    (direction === 'up')? gMeme.txts[txtIdx].y-- : gMeme.txts[txtIdx].y++;
+    let txt = gMeme.txts[txtIdx];
+    (direction === 'up') ? (txt.y -= 5) : (txt.y += 5);
 }
+
+let gImgs = [{ id: 1, url: 'img/1.jpg' }, 
+             { id: 2, url: 'img/2.jpg' },
+             { id: 3, url: 'img/3.jpg' },
+             { id: 4, url: 'img/4.jpg' },
+             { id: 5, url: 'img/5.jpg' },
+             { id: 6, url: 'img/6.jpg' },
+             { id: 7, url: 'img/7.jpg' },
+             { id: 8, url: 'img/8.jpg' },
+             { id: 9, url: 'img/9.jpg' },
+             { id: 10, url: 'img/10.jpg' },
+             { id: 11, url: 'img/11.jpg' },
+             { id: 12, url: 'img/12.jpg' },
+             { id: 13, url: 'img/13.jpg' },
+             { id: 14, url: 'img/14.jpg' },
+             { id: 15, url: 'img/15.jpg' },
+             { id: 16, url: 'img/16.jpg' },
+             { id: 17, url: 'img/17.jpg' },
+             { id: 18, url: 'img/18.jpg' },
+             { id: 19, url: 'img/19.jpg' },
+             { id: 20, url: 'img/20.jpg' },
+             { id: 21, url: 'img/21.jpg' },
+             { id: 22, url: 'img/22.jpg' },
+             { id: 23, url: 'img/23.jpg' },
+             { id: 24, url: 'img/24.jpg' },
+            //  { id: 25, url: 'img/25.jpg' },
+            ];
